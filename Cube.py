2f31c881ba8b065520d9d468ac81f8bdde67e0e4
jstammers <jimmy.stammers@gmail.com>
2017-03-20 00:13:23 +0000

    Refactored rotate_cube to make it work for any sized cube. Runs slightly slower but should not be a bottleneck

--- Cube.py
+++ Cube.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as npimg
 class RubiksCube:
     
     def __init__(self,n=3):
@@ -20,54 +23,34 @@ class RubiksCube:
     def _initialize(self,n):
         return self.solved_cube.copy()
     
-    def rotate_cube(self,face,n):
-        if face not in self.facesdict.keys():
-            raise "Incorrect Face Specified"
-        indices = self.rotation_indices(face)
-        if n == 1:
-            ix = [1,2,3,0]
-            k = 1
-        elif n == -1:
-            ix = [3,0,1,2]
-            k=3
-        sides = self.get_adjacent_squares(indices)
-        self.cube[self.facesdict[face]]=np.rot90(self.cube[self.facesdict[face]],k=k)
-        self.set_adjacent_sqaures(indices,sides[np.ix_(ix)])
+    def rotate_cube(self,index,axis,d):
+        rot = False
+        if axis == 0:
+            if index == 0:
+                face_ix = 1
+                rot = True
+            elif index == self.n-1:
+                face_ix = 2
+                rot = True
+            #horizontal
+            ix = [0,3,5,4]
+            sides = np.vstack(self.cube[ix]).T
+            sides[index] = np.roll(sides[index],d*3)
+            self.cube[ix] = np.vsplit(sides.T,4)
+        elif axis == 1:
+            if index == 0:
+                face_ix = 3
+                rot = True
+            elif index == self.n-1:
+                face_ix = 4
+            #vertical
+            ix = [0,1,5,2]   
+            sides = np.hstack(self.cube[ix])
+            sides[index] = np.roll(sides[index],d*3)
+            self.cube[ix] = np.hsplit(sides,4)
+            
+        if rot: ar[face_ix] = np.rot90(slef.cube[face_ix],k=d)
         
-        
-    def rotation_indices(self,face):
-        if face == "F":
-            sides = [("U",0,1),("R",0,1),("D",self.n-1,1),("L",0,1)]
-        elif face == "U":
-            sides = [("B",0,1),("R",0,0),("F",self.n-1,1),("L",self.n-1,0)]
-        elif face == "D":
-            sides = [("F",0,1),("R",self.n-1,0),("B",self.n-1,1),("L",0,0)]
-        elif face == "L":
-            sides = [("U",0,0),("F",0,0),("D",0,0),("B",0,0)]
-        elif face == "R":
-            sides =  [("U",self.n-1,0),("B",self.n-1,0),("D",self.n-1,0),("F",self.n-1,0)]
-        else:
-            sides = [("U",self.n-1,1),("R",self.n-1,1),("D",0,1),("L",self.n-1,1)]
-        return sides
-    
-    def get_adjacent_squares(self,indices):
-        '''
-        Returns the squares that are adjacent to a given face. The faces are defined in a clockwise order, starting from the face above the given one. Each side is the index of the required slice, along with the axis
-        '''
-        l = []
-        for face,ix,ax in indices:
-            if ax == 1:
-                l.append(self.cube[self.facesdict[face],:,ix])
-            else:
-                l.append(self.cube[self.facesdict[face],ix])
-        return np.array(l)
-    
-    def set_adjacent_sqaures(self,indices,values):
-        for i,(face,ix,ax) in enumerate(indices):
-            if ax == 1:
-                self.cube[self.facesdict[face],:,ix] = values[i]
-            else:
-                self.cube[self.facesdict[face],ix] = values[i]
                 
     def score_similarity(self):
         a1 = self.cube.ravel()
